00b1818bea07367de9cc0a132c5c2e9d6e94d430
tomer_cool <tomer.kotzer@gmail.com>
2020-10-27 19:01:46 +0200

    Some changes to home page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.friendContainer').forEach(container => {
-        container.onclick = () => {
-            console.log('wow ');
-            container.classList.toggle('selected');
-        }
-    })
+	document.querySelectorAll('.friendContainer').forEach(container => {
+		container.onclick = () => {
+			container.classList.toggle('selected');
+		}
+	});
+
+	const dom = {
+		halfs: {
+			left: document.querySelector('#leftContainer'),
+			right: document.querySelector('#rightContainer'),
+		}
+	};
+	
+	dom.halfs.left.onmouseenter = () => {
+		dom.halfs.left.classList.add('expendedContainer');
+		dom.halfs.right.classList.remove('expendedContainer');
+	}
+
+	dom.halfs.right.onmouseenter = () => {
+		dom.halfs.left.classList.remove('expendedContainer');
+		dom.halfs.right.classList.add('expendedContainer');
+	}
 });
